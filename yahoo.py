14fb64cfaa5160d3127a84ab43818217872d8d29
vadym <kovalchuk.vadim@gmail.com>
2021-05-07 23:31:15 +0300

    finalize ticker price history

--- yahoo.py
+++ yahoo.py
@@ -1,5 +1,4 @@
 import json
-from typing import Union
 
 import yfinance
 
@@ -7,31 +6,14 @@ from pandas import DataFrame
 
 START_DATE = '2016-01-01'
 
-def pd_to_dict(dataframe: DataFrame):
-    pass
 
-
-def get_ticker_data(parameters: dict):
-                    # ticker: str,
-                    # method_name: str,
-                    # arguments: dict = {},
-                    # post_method_name: str = ''):
-    tkr = yfinance.Ticker(parameters['ticker'])
-    attribute = getattr(tkr, parameters['method_name'])
-    if isinstance(attribute, type(tkr.get_calendar)):
-        result = attribute()
-    else:
-        result = attribute
-    if parameters.get('post_method_name'):
-        post_method = getattr(result, parameters['post_method_name'])
-        return post_method()
-    else:
-        return result
-
-
-def ticker_history(ticker: str, start: str = START_DATE):
-    tkr = yfinance.Ticker(ticker)
-    history = tkr.history(start=start)
+def ticker_history(arg_dict: dict):
+    if 'ticker' not in arg_dict:
+        raise AttributeError('"ticker" key is missing in arguments dict')
+    tkr = yfinance.Ticker(arg_dict['ticker'])
+    if 'start' not in arg_dict:
+        arg_dict['start'] = START_DATE
+    history = tkr.history(start=arg_dict['start'])
     history.reset_index(inplace=True)
     history['Date'] = history['Date'].apply(lambda x: x.isoformat()[:10])
     result = {
@@ -41,15 +23,21 @@ def ticker_history(ticker: str, start: str = START_DATE):
     }
     for indx in range(history.shape[0]):
         row = history.loc[indx]
-        result['prices'].append((row['Date'], row['Close']))
+        result['prices'].append((row['Date'],
+                                 row['Open'],
+                                 row['High'],
+                                 row['Low'],
+                                 row['Close'],
+                                 float(row['Volume'])))
         if row['Dividends']:
             result['dividends'].append((row['Date'], row['Dividends']))
         if row['Stock Splits']:
             result['splits'].append((row['Date'], row['Stock Splits']))
-    return result
+    return json.dumps(result)
 
 
 if __name__ == '__main__':
-    res = ticker_history("MSFT")
-    print(json.dumps(res))
+    res = ticker_history({'ticker': "MSFT", 'start': '2021-03-14'})
+    print(res)
+    print(json.dumps(json.loads(res), indent=4))
 
