042f76dc11789de82220bf906b55b452f08c74d1
Vadym Kovalchuk <kovalchuk.vadim@gmail.com>
2023-03-21 20:44:21 +0200

    refactor ticker scope getter

--- findus_edge/tickers.py
+++ findus_edge/tickers.py
@@ -2,9 +2,11 @@ import json
 
 import pandas as pd
 
-LARGE_CAP = ('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', 0, 'Symbol')
-MID_CAP = ('https://en.wikipedia.org/wiki/List_of_S%26P_400_companies', 0, 'Symbol')
-SMALL_CAP = ('https://en.wikipedia.org/wiki/List_of_S%26P_600_companies', 0, 'Symbol')
+SCOPES = {
+    'SP500': ('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', 0, 'Symbol'),
+    'SP400': ('https://en.wikipedia.org/wiki/List_of_S%26P_400_companies', 0, 'Symbol'),
+    'SP600': ('https://en.wikipedia.org/wiki/List_of_S%26P_600_companies', 0, 'Symbol')
+}
 
 
 def getter(link: str, section: int, key: str):
@@ -14,21 +16,15 @@ def getter(link: str, section: int, key: str):
     return json.dumps(lst)
 
 
-def get_sp500_ticker_list():
-    return getter(*LARGE_CAP)
-
-
-def get_sp400_ticker_list():
-    return getter(*MID_CAP)
-
-
-def get_sp600_ticker_list():
-    return getter(*SMALL_CAP)
+def get_scope(args: dict):
+    scope = args.get('scope')
+    assert scope, 'Scope is not defined'
+    return getter(*scope)
 
 
 def main():
-    for link, section, key in (LARGE_CAP, MID_CAP, SMALL_CAP):
-        print((getter(link, section, key)))
+    for _, scope in SCOPES.items():
+        print(getter(*scope))
 
 
 if __name__ == '__main__':
