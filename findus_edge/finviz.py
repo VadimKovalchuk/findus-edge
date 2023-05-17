d7bf7fa63bb788c5cf94ea8518b0ab0bdab2ae59
Vadym Kovalchuk <kovalchuk.vadim@gmail.com>
2023-05-17 20:04:03 +0300

    bump finviz

--- findus_edge/finviz.py
+++ findus_edge/finviz.py
@@ -127,5 +127,5 @@ def fundamental_converted(arg_dict: dict):
 if __name__ == '__main__':
     # res = fundamental(["MSFT", "O"])
     # print(json.dumps(json.loads(res), indent=4))
-    res = fundamental_converted({'ticker': "O"})
+    res = fundamental_converted({'ticker': "MSFT"})
     print(json.dumps(json.loads(res), indent=4))
